1cb9d7a9f323e994b5d74cc22f59201e1189ad51
agungibr <agungibr@outlook.co.id>
2023-10-12 10:38:59 +0700

    screens

---
+++ screens/quiz.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const Quiz = () => {
+    return (
+        <View>
+            <Text>This is Quiz</Text>
+        </View>
+    );
+};
+
+export default Quiz;
+
+const styles = StyleSheet.create({});
